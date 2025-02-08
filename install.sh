841b7eddebb9246e53e8b9692fa60213ea90cce0
jmccoy555 <james@mcy.email>
2025-02-08 08:03:08 +0000

    Update install.sh
    updated openautoRepo

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 #repo addresses
 aasdkRepo="https://github.com/OpenDsh/aasdk"
 gstreamerRepo="https://github.com/GStreamer/qt-gstreamer"
-openautoRepo="https://github.com/openDsh/openauto"
+openautoRepo="https://github.com/jmccoy555/openauto"
 h264bitstreamRepo="https://github.com/aizvorski/h264bitstream"
 pulseaudioRepo="https://gitlab.freedesktop.org/pulseaudio/pulseaudio.git"
 
